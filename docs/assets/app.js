2c43e0898e7d266101e73cf8773685a33493ad47
Redxjak <Redxjak@gmail.com>
2026-05-27 20:37:08 +0900

    Bump web asset cache version

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  const VERSION = "0.7.7";
+  const VERSION = "0.7.8";
   const MAP_DIRECTIONS = ["LEFT", "UP", "RIGHT", "DOWN"];
   const BASE_LEVEL = 5;
   const BASE_XP_TO_NEXT = 100;
